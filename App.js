c617b261a633da80229518cb6a47475772c6b9db
cledson <cledman@gmail.com>
2020-12-18 13:43:32 -0300

    Eslint and prettier configurated.

--- App.js
+++ App.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
-
-export default function App(){
-  return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Hello, DIO!
-        </Text>
-      </View>
-    </>
-  );
-};
+import {StyleSheet, View, Text} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     backgroundColor: '#f5f5f5',
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
-  }, 
+  },
   title: {
-    color: "#000",
+    color: '#000',
     fontSize: 24,
     margin: 10,
     textAlign: 'center',
   },
-
 });
 
-
+export default function App() {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello, DIO!</Text>
+      </View>
+    </>
+  );
+}
